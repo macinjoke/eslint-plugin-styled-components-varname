b3cf3df933d7011e8309afc53dd4926433bc1cc7
macinjoke <shunji.makino@gmail.com>
2019-12-25 14:00:53 +0900

    manual update committed by autofix branch

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,20 @@
+const rulesDirPlugin = require('eslint-plugin-rulesdir')
+rulesDirPlugin.RULES_DIR = 'lib/rules'
+
 module.exports = {
   env: {
     node: true,
+    es6: true,
   },
-  extends: ['plugin:prettier/recommended', 'prettier'],
-  plugins: ['prettier'],
+  extends: ['plugin:prettier/recommended', 'eslint:recommended', 'prettier'],
+  plugins: ['prettier', 'rulesdir'],
   parserOptions: {
-    ecmaVersion: 6,
+    ecmaVersion: 8,
     sourceType: 'module',
   },
   rules: {
     'func-style': ['error', 'declaration', { allowArrowFunctions: true }],
     'prefer-arrow-callback': 2,
+    'rulesdir/varname': 2,
   },
 }
